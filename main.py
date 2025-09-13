bff155e17a7beba8e1ec0fb33309e756dbbb427e
Dhruv Vaghani <108922829+DhruvVaghani@users.noreply.github.com>
2025-09-13 11:49:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Or replace with specific origin like ["http://localhost:5173"]
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
